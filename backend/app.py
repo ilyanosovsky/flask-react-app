68c70e660a49e15c3dc55c41452a3f993e6fd65c
ilyanosovsky <imnosovsky@gmail.com>
2023-08-29 17:54:51 +0300

    new app

---
+++ backend/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ilyanosovsky:1235846Qq@localhost:5432/flask-test'
+app.config['JWT_SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)  # Changed from username to email
+    password = db.Column(db.String(128), nullable=False)
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    print("data ----> ", data)
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(email=data['email'], password=hashed_password)  # Modified here
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created!"}), 201
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    data = request.get_json()
+    user = User.query.filter_by(email=data['email']).first()  # Modified here
+    if not user or not check_password_hash(user.password, data['password']):
+        return jsonify({"message": "Invalid credentials!"}), 401
+    access_token = create_access_token(identity=user.email)  # Use email as identity
+    return jsonify({"access_token": access_token})
+
+@app.route('/users')
+def get_users():
+    users = User.query.order_by(User.id.asc()).all()
+    return {"users": users}
+
+@app.route('/users/<id>')
+def get_user(id):
+    user = User.query.filter_by(id=id).one()
+    return {"user": user}
+
+@app.route('/users/<id>', methods = ['DELETE'])
+def delete_user(id):
+    user = User.query.filter_by(id=id).one()
+    db.session.delete(user)
+    db.session.commit()
+    return f'User (id: {id}) deleted!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
