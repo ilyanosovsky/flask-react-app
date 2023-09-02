9d322f2b9171fe8643d229604a5887964b22ca2d
ilyanosovsky <imnosovsky@gmail.com>
2023-09-02 17:33:51 +0300

    can create orgs and add users

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Paper } from '@mui/material';
+import { Container, Typography, Paper, TextField, Button, Select, MenuItem } from '@mui/material';
 import axios from 'axios';
 
 function HomePage() {
     const [organizations, setOrganizations] = useState([]);
     const [users, setUsers] = useState([]);
+    const [newOrgName, setNewOrgName] = useState('');
+    const [selectedOrgId, setSelectedOrgId] = useState(null);
+    const [selectedUserId, setSelectedUserId] = useState(null);
 
     useEffect(() => {
         // Fetch organizations and users when the component mounts
         const fetchOrganizations = async () => {
             try {
-                const response = await axios.get('/organizations'); // Adjust the endpoint
-                setOrganizations(response.data.organizations); // Make sure to access the 'organizations' key in the response data
+                const response = await axios.get('/organizations');
+                setOrganizations(response.data.organizations);
             } catch (error) {
                 console.error('Error fetching organizations:', error);
             }
@@ -20,14 +23,12 @@ function HomePage() {
         const fetchUsers = async () => {
             try {
                 const token = localStorage.getItem("token");
-                console.log("Fetching users...");
                 const response = await axios.get("/users", {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
                 });
-                console.log("Fetched users:", response.data.users);
-                setUsers(response.data.users); // Set the users state with the fetched users
+                setUsers(response.data.users);
             } catch (error) {
                 console.error("Error fetching users:", error);
             }
@@ -37,21 +38,103 @@ function HomePage() {
         fetchUsers();
     }, []);
 
+    const handleCreateOrganization = async () => {
+        try {
+            const response = await axios.post('/organizations', {
+                name: newOrgName,
+            });
+            setOrganizations([...organizations, response.data.organization]);
+            setNewOrgName('');
+        } catch (error) {
+            console.error('Error creating organization:', error);
+        }
+    };
+
+    const handleAddUserToOrganization = async () => {
+        try {
+            if (selectedOrgId && selectedUserId) {
+                const response = await axios.post(`/organizations/${selectedOrgId}/add_user`, {
+                    email: users.find(user => user.id === selectedUserId).email,
+                });
+                setUsers([...users, response.data.user]);
+                setSelectedOrgId(null); // Reset selected organization
+                setSelectedUserId(null); // Reset selected user
+            }
+        } catch (error) {
+            console.error('Error adding user to organization:', error);
+        }
+    };
+
     return (
-        <Container maxWidth="md">
+        <Container sx={{ display:"flex", flexDirection:"row-reverse", padding: "1rem 0rem"}}>
+            <Container >
             <Typography variant="h4">Organizations</Typography>
-            {organizations.map((org) => (
-                <Paper key={org.id} elevation={3} style={{ padding: '10px', margin: '10px 0' }}>
-                    <Typography variant="h5">{org.name}</Typography>
-                    {org.users.map((user) => (
-                        <Typography key={user.id}>{user.email}</Typography>
+                {organizations.length > 0 ? (
+                    organizations.map((org) => (
+                        <Paper key={org.id} elevation={3} style={{ padding: '10px', margin: '10px 0' }}>
+                            <Typography variant="h5">{org.name}</Typography>
+                            {org.users.map((user) => (
+                                <Typography key={user.id}>{user.email}</Typography>
+                            ))}
+                        </Paper>
+                    ))
+                ) : (
+                    <Typography>No organizations available.</Typography>
+                )}
+            </Container>
+
+            <Container>
+                <TextField
+                    label="Organization Name"
+                    variant="outlined"
+                    value={newOrgName}
+                    onChange={(e) => setNewOrgName(e.target.value)}
+                />
+                <Button variant="contained" onClick={handleCreateOrganization}>
+                    Create Organization
+                </Button>
+
+                <hr />
+
+                {/* Dropdown for selecting organization */}
+                <Select
+                    label="Select Organization"
+                    value={selectedOrgId}
+                    onChange={(e) => setSelectedOrgId(e.target.value)}
+                    variant="outlined"
+                    style={{ marginTop: '10px' }}
+                >
+                    <MenuItem value="" disabled>
+                        Select Organization
+                    </MenuItem>
+                    {organizations.map((org) => (
+                        <MenuItem key={org.id} value={org.id}>
+                            {org.name}
+                        </MenuItem>
                     ))}
-                </Paper>
-            ))}
-            <Typography variant="h4">Users</Typography>
-            {users.map((user) => (
-                <Typography key={user.id}>{user.email}</Typography>
-            ))}
+                </Select>
+
+                {/* Dropdown for selecting user */}
+                <Select
+                    label="Select User"
+                    value={selectedUserId}
+                    onChange={(e) => setSelectedUserId(e.target.value)}
+                    variant="outlined"
+                    style={{ marginTop: '10px' }}
+                >
+                    <MenuItem value={null}>Select User</MenuItem>
+                    {users.map((user) => (
+                        <MenuItem key={user.id} value={user.id}>
+                            {user.email}
+                        </MenuItem>
+                    ))}
+                </Select>
+
+                <Button variant="contained" onClick={handleAddUserToOrganization}>
+                    Add User to Organization
+                </Button>
+            </Container>        
+
         </Container>
     );
 }
